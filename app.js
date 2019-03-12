9b535d9290b683045297e3cc56864d4bf9792bbf
Chris Ivey <chrisivey01@gmail.com>
2019-03-12 08:43:46 -0500

    port issues

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 // grab the packages we need
 const express = require("express");
 const app = express();
-const port = process.env.PORT || 8000;
+const port = 8000;
 
 app.use(function(req, res, next) {
   res.header("Access-Control-Allow-Origin", "*");
